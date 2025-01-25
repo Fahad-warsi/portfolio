27b7f66e31a787641c0672cd273263d512e32ef0
Fahad-warsi <fahad7swarsi@gmail.com>
2025-01-25 10:34:36 +0500

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 
 const Home: React.FC = () => {
@@ -31,7 +32,7 @@ const Home: React.FC = () => {
               <p className="mt-2">
 Description: I created a static resume builder using HTML and CSS. This project showcases my personal information, work experience, education, and skills in a clean and professional format.
 </p>
-Link: <a href="https://hackathon-resume-builder-opal.vercel.app/" target="_blank" rel="noopener noreferrer"><b>View My Resume</b></a>.
+Link: <Link href="https://hackathon-resume-builder-opal.vercel.app/" target="_blank" rel="noopener noreferrer"><b>View My Resume</b></Link>.
             </div>
             <div className="project bg-white p-6 rounded shadow">
               <h3 className="text-2xl font-bold">Project 2:Dynamic and Editable Resume Builder</h3>
@@ -39,13 +40,13 @@ Link: <a href="https://hackathon-resume-builder-opal.vercel.app/" target="_blank
               <p className="mt-2">Description: Developed a dynamic and editable resume builder using HTML and CSS. 
                 This project allows users to create and customize their resumes interactively,
                  showcasing their personal information, work experience, education, and skills.</p>
-                 Link:<a href='https://editable-resume-builder-ybvw-ad5nt3mw0-fahad-warsis-projects.vercel.app/' target="_blank" rel="noopener noreferrer">  <b>View My Resume</b></a>
+                 Link:<Link href='https://editable-resume-builder-ybvw-ad5nt3mw0-fahad-warsis-projects.vercel.app/' target="_blank" rel="noopener noreferrer">  <b>View My Resume</b></Link>
             </div>
             <div className="project bg-white p-6 rounded shadow">
               <h3 className="text-2xl font-bold">Project 3: Editable and Shareable Resume Builder</h3>
               <p className="mt-2">Description: Created an editable and shareable resume builder. This project enables users to customize their resumes,
                  share them with anyone, and download them as PDFs for convenience.</p>
-                 Link:<a href='https://hackathon-resume-builder-b6l5.vercel.app/' target="_blank" rel="noopener noreferrer">  <b>View My Resume</b></a>
+                 Link:<Link href='https://hackathon-resume-builder-b6l5.vercel.app/' target="_blank" rel="noopener noreferrer">  <b>View My Resume</b></Link>
             </div>
           </div>
         </section>
